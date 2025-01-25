d567a301e31b030440b186c7494c0ef31bf1bc2f
Asus <gienadii966@gmail.com>
2025-01-25 16:11:15 +0300

    change int_orders_enrished_hmw2_Zubkov.sql in int

--- my_dbt_project/models/int/int_orders_enrished_hmw2_Zubkov.sql
+++ my_dbt_project/models/int/int_orders_enrished_hmw2_Zubkov.sql
@@ -3,7 +3,8 @@ select
    orders.customer_id,
    orders.order_date,
    orders.ship_country,
-   details.order_value
+   details.order_value,
+   {{ continent('orders.ship_country')}} as continent
 from {{ ref('sales__orders')}} as orders
 left join {{ ref('int_orders_values_hmw2_Zubkov')}} as details
    on orders.order_id = details.order_id
